7e04e7c7e7dc1f760bb76421fbce0ed1976f9b76
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-14 14:13:40 -0500

    employee array displays

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const employees = require('./employess.js');
+
 const express = require('express');
 const app = express();
 const port = 8081;
@@ -5,6 +7,10 @@ const port = 8081;
 app.get ('/', (req, res) => {
   res.send('Hello employees!');
 });
+
+app.get ('/employees', (req,res) => {
+  res.send(employees);
+})
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`)

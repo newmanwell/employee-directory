8a7047bbdbe35d6acd7fbcd8ad4dc8de356fcc7e
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-14 15:40:11 -0500

    single employee shows up with the id variable

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello employees!');
 });
 
-app.get ('/employees', (req,res) => {
+app.get('/employees', (req, res) => {
   res.send(employees);
+})
+
+app.get('/employees/:id', (req, res) => {
+  const { id } = req.params;
+
+  const employeeId = employees.find((oneEmployee) => {
+    return oneEmployee.id === Number(id);
+  })
+  res.send(employeeId);
 })
 
 app.listen(port, () => {
